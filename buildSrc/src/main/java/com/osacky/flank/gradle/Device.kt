17901aac6f16470b2523223fa1a789438dc1e4d7
Jeroen Mols <jmols.mobile@gmail.com>
2019-06-26 11:15:49 +0200

    Add secondary device constructor to select Android beta versions.
    E.g. Pixel 3 (blueline) can also run tests on Q-beta-3, but that cannot be configured with an int Device constructor.
    In order not to break the API I decided to add a secondary constructor.

--- buildSrc/src/main/java/com/osacky/flank/gradle/Device.kt
+++ buildSrc/src/main/java/com/osacky/flank/gradle/Device.kt
@@ -1,3 +1,6 @@
 package com.osacky.flank.gradle
 
-data class Device(val model: String, val version: Int, val orientation: String? = null, val locale: String? = null)
+data class Device(val model: String, val version: String, val orientation: String? = null, val locale: String? = null) {
+    constructor(model: String, version: Int, orientation: String? = null, locale: String? = null) :
+            this(model, "$version", orientation, locale)
+}
