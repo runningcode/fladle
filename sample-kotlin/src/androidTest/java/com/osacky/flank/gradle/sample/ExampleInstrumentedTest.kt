081b9809853609f198d4bbd3a76ebf007fce45cb
Nelson Osacky <nelson@osacky.com>
2020-05-16 00:22:12 +0200

    Fix compilation.
    Also fix compiling samples on CI.

--- sample-kotlin/src/androidTest/java/com/osacky/flank/gradle/sample/ExampleInstrumentedTest.kt
+++ sample-kotlin/src/androidTest/java/com/osacky/flank/gradle/sample/ExampleInstrumentedTest.kt
@@ -1,19 +1,16 @@
 package com.osacky.flank.gradle.sample
 
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.osacky.flank.gradle.sample.kotlin.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
-
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.action.ViewActions.typeText
-import java.lang.RuntimeException
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
