d33223104a11446ea9b64868d64cc81ff50e36c1
Nelson Osacky <nelson@osacky.com>
2020-08-28 00:43:39 +0200

    Prepare for next development iteration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 group = "com.osacky.flank.gradle"
-version = "0.12.0"
+version = "0.12.1-SNAPSHOT"
 description = "Easily Scale your Android Instrumentation Tests with Firebase Test Lab with Flank"
 
 repositories {
