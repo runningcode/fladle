160f0821991509cd5b3507c12b165c139a9c28ed
Pawel Pasterz <pawel.pasterz@gmail.com>
2020-10-22 08:59:38 +0200

    Refactor

--- buildSrc/src/test/java/com/osacky/flank/gradle/integration/AutoConfigureFladleTest.kt
+++ buildSrc/src/test/java/com/osacky/flank/gradle/integration/AutoConfigureFladleTest.kt
@@ -5,6 +5,7 @@ import org.gradle.testkit.runner.GradleRunner
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
+import java.io.File
 
 class AutoConfigureFladleTest {
 
@@ -82,5 +83,25 @@ class AutoConfigureFladleTest {
           output-style: single
       """.trimIndent()
     )
+
+    GradleRunner.create()
+      .withProjectDir(testProjectRoot.root)
+      .withPluginClasspath()
+      .withArguments("runFlankOranges", "-PdumpShards")
+      .build()
+
+    assertThat(
+      File(testProjectRoot.root.absolutePath + "/android-project/build/fladle/oranges/android_shards.json").exists()
+    ).isTrue()
+
+    GradleRunner.create()
+      .withProjectDir(testProjectRoot.root)
+      .withPluginClasspath()
+      .withArguments("runFlankFlash")
+      .buildAndFail()
+
+    assertThat(
+      File(testProjectRoot.root.absolutePath + "/android-project/build/fladle/flash/flank.yaml").exists()
+    ).isTrue()
   }
 }
