32e63c60448404c228be959fb8f0c245d5e43dc6
Nelson Osacky <nosacky@gradle.com>
2021-01-17 16:29:06 +0100

    Use Central Dependency Declaration (#210)

--- sample-kotlin/build.gradle.kts
+++ sample-kotlin/build.gradle.kts
@@ -55,10 +55,10 @@ fladle {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
-    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test:rules:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
