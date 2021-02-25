92e5f9329a9420c3c0c3ae23007973ca0d948564
Asad Salman <me@asad.co>
2021-02-24 17:31:25 -0800

    Update build.gradle.kts

--- fladle-plugin/build.gradle.kts
+++ fladle-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 group = "com.osacky.flank.gradle"
-version = "0.14.2-SNAPSHOT"
+version = "0.15.0-SNAPSHOT"
 description = "Easily Scale your Android Instrumentation Tests with Firebase Test Lab with Flank"
 
 repositories {
