20ce0da510b7f7ac9e61595239eee168ac1bf491
Nelson Osacky <nelson@osacky.com>
2021-04-06 22:25:29 +0200

    Bump flank to 21.04.0

--- sample-kotlin/build.gradle.kts
+++ sample-kotlin/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 }
 
 fladle {
-    flankVersion.set("21.03.1")
+    flankVersion.set("21.04.0")
     // Project Id is not needed if serviceAccountCredentials are set.
     projectId.set("flank-gradle")
     useOrchestrator.set(true)
