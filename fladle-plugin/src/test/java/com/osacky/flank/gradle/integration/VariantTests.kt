c382ee142e26f69085e167c177181d9a7baaf714
Nelson Osacky <nosacky@gradle.com>
2023-10-23 00:30:26 +0200

    Update more buildscripts

--- fladle-plugin/src/test/java/com/osacky/flank/gradle/integration/VariantTests.kt
+++ fladle-plugin/src/test/java/com/osacky/flank/gradle/integration/VariantTests.kt
@@ -140,11 +140,12 @@ class VariantTests {
               mavenCentral()
           }
          android {
-             compileSdkVersion 29
+             compileSdk 29
+             namespace = "com.osacky.flank.gradle.sample"
              defaultConfig {
                  applicationId "com.osacky.flank.gradle.sample"
-                 minSdkVersion 23
-                 targetSdkVersion 29
+                 minSdk 23
+                 targetSdk 29
                  versionCode 1
                  versionName "1.0"
                  testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
